4b7257f9e88ab7fc4ee142bdc560184724777cf6
BrayanMaximiano <brayanmaximiano01@gmail.com>
2022-05-07 15:44:31 -0300

    correcao bug cors

--- src/server.ts
+++ src/server.ts
@@ -8,7 +8,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
-app.use(routes)''
+app.use(routes)
 
 app.listen(process.env.PORT || 3333, () => {
     console.log('HTTP server running!');
